6f20b85ed80a5fb0d3a88cc8a9f4f6acf60c5bfd
leo <leonardo.stefani.ge@gmail.com>
2014-01-25 18:31:35 +0100

    Messo EnumUtils in un file separato

--- U/U.cs
+++ U/U.cs
@@ -161,30 +161,4 @@ public static partial class U
 		if (ret.Count > 0)
 			yield return ret;
 	}
-
-	public static class EnumUtils
-	{
-		public static bool IsDefined<T>(string enumString) where T : struct
-		{
-			if (String.IsNullOrEmpty(enumString))
-				throw new NullReferenceException(enumString);
-			if (typeof(T).IsEnum == false)
-				throw new ArgumentException("Type given must be an Enum", "T");
-			return Enum.IsDefined(typeof(T), enumString);
-		}
-		public static T ParseEnum<T>(string enumString) where T : struct
-		{
-			if (String.IsNullOrEmpty(enumString))
-				throw new NullReferenceException(enumString);
-			if (typeof(T).IsEnum == false)
-				throw new ArgumentException("Type given must be an Enum", "T");
-			return (T)Enum.Parse(typeof(T), enumString, true);
-		}
-		public static string[] GetNames<T>() where T : struct
-		{
-			if (typeof(T).IsEnum == false)
-				throw new ArgumentException("Type given must be an Enum", "T");
-			return Enum.GetNames(typeof(T));
-		}
-	}
 }
